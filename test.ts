c636998f07925a9237e074dca9b6c158ec62e481
Pascal Eberhard <35179162+paeber@users.noreply.github.com>
2025-01-08 16:09:23 +0100

    Update test.ts to check functionality

--- test.ts
+++ test.ts
@@ -9,31 +9,31 @@
  * --
  * -- File:	    test.ts
  * -- Project:  micro:bit InES Matrix
- * -- Date:	    16.12.2024
- * -- Author:   hesu
+ * -- Date:	    08.01.2024
+ * -- Author:   ebep
  * --
+ * -- Description:  Illustrate and test core functionality
  * ------------------------------------------------------------------
  */
 
-Lumatrix.switchValueChangedThread(function () {
-    basic.showNumber(Lumatrix.readSwitch())
-    serial.writeValue("switch", Lumatrix.readSwitch())
-    Lumatrix.setCurrentTime(h, m, s)
-})
-let s = 0
-let m = 0
-let h = 0
+
 Lumatrix.debugEnable(true)
 Lumatrix.initializeMatrix(DigitalPin.P0, 135)
-Lumatrix.createWordClock(
-eMatrixVersion.V1,
-0x00ff00,
-0x007fff,
-0xff0000
-)
-h = 0
-m = 22
-s = 55
+Lumatrix.scrollText("LUMATRIX", 0xff00FF, 90)
+Lumatrix.showImage(Lumatrix.matrix8x8(`
+    . . . . . . . .
+    . # # . . # # .
+    . # # . . # # .
+    . . . . . . . .
+    . # . . . . # .
+    . # . . . . # .
+    . . # # # # . .
+    . . . . . . . .
+    `), 0xffff00)
+basic.pause(2000)
+Lumatrix.createWordClock(eMatrixVersion.V1, 0xff00ff, 0x00ffff, 0xffff00)
+Lumatrix.setCurrentTime(15, 33, 0)
+
 while (true) {
     basic.pause(5000)
     serial.writeLine(Lumatrix.getCurrentTimeAsText())
